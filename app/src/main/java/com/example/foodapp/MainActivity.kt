3add8452976b11eb0eb932700a82ebc6eba86394
Ngô Văn Dũng <ngovandung1552003@gmail.com>
2025-11-23 23:13:02 +0700

    update: add file

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FoodAppTheme {
-        Greeting("Android")
+        Greeting("Android App")
     }
 }
